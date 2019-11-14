421168e9d8ec39c6cf55ba9c41ae86057959afe2
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-14 17:08:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,6 @@ if grandma = "can hear"
   puts "THE WEATHER IS NICE TODAY"
 elsif grandma = "cant hear"
 puts "NO! NOT SINCE 1983!"
-
+else
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
